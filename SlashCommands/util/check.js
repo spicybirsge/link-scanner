83c85bb1ee8789769b1a9721c98a8449db3105c8
spicybirsge <shaheer.myhome@gmail.com>
2022-03-19 10:13:12 +0530

    Added more info in check command

--- SlashCommands/util/check.js
+++ SlashCommands/util/check.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch')
-
+const {MessageEmbed, Message} = require("discord.js")
 module.exports = {
     name: 'check',
-    description: "Check if a domain is a know scam domain or not",
+    description: "Check if a domain is a known scam domain or not",
     options: [
       {
         name: 'domain',
@@ -16,6 +16,8 @@ module.exports = {
    
     run: async (client, interaction) => {
         const domain = interaction.options.getString('domain')
+        const regex =   /([-a-zA-Z0-9_-]{2,256}\.[a-z]{2,10})(?:\/(\S*))?\b/g;
+        if(!regex.test(domain)) return interaction.followUp({content: ":x: No domain was found in this command."})
         const f = await fetch('https://anti-fish.bitflow.dev/check', {
                     method: 'POST',
                     body: JSON.stringify({
@@ -26,16 +28,25 @@ module.exports = {
                         "User-Agent": "Link Scanner/test-build",
                     
                     }
-                });
+                }).then(res => res.json()).then(async(data) => {
+                    
 
-        const status = f.status
-        if(status === 200) {
-            return interaction.followUp({content: ":white_check_mark: This is a known scam domain."})
+       
+                    if(data.match) {
+            const scam111 = new MessageEmbed()
+            .setTitle(":warning: Scam Domain Found")
+            .addField("Name:", data.matches[0].domain || "Was Unable to get the data :(")
+            .addField("Type of Scam:", data.matches[0].type.toLowerCase().replace("_", " ") || "Was Unable to get the data :(")
+            .addField("Redirect?", data.matches[0].followed ? "Yes" : "No" || "Was Unable to get the data :(")
+            .setColor("RED")
+            .setThumbnail("https://cdn.discordapp.com/attachments/943887879078297610/954598948859961375/Z.png")
+            return interaction.followUp({embeds: [scam111]})
 
         } else {
             return interaction.followUp({content: ":x: This is a not known scam domain."})
 
         }
+    })
 
     }
 }
